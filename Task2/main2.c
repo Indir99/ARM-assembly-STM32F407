eb13b1831acf32733ad336a9569af48a89ffcc23
indir <indir.karabegovic@hotmail.com>
2022-04-25 16:30:31 +0200

    task2 design

--- Task2/main2.c
+++ Task2/main2.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-
-int main(){
-    char *poruka="KAKO STE DANAS";
-    int i=0;
-    while(poruka[i]!='\0'){
+int main()
+{
+    char *message = "HOW ARE YOU?";
+    int i = 0;
+    while (message[i] != '\0')
+    {
         i++;
     }
-    printf("Vrijednost brojaca je: %d ",i);
+    printf("Counter value: %d ", i);
     printf("\n");
 
     return 0;
--- Task2/main2.s
+++ Task2/main2.s
@@ -7,22 +7,20 @@
 .global main
 
 	   .data
- poruka:  .asciz "KAKO STE DANAS"  //Odg. tekst smjestimo u memoriju na neku adresu
+ message:  .asciz "HOW ARE YOU?"  // Saving message on memory address
 	   .text
 
 
 main:
-	ldr r0,=poruka      //Dohvatamo adresu teksta i smjestimo je u registar R0
-	mov r1,#0			//Cistimo registar R1
-	mov r2,#0			//Pomocni registar koji broji koliko smo puta bili u petlji
+	ldr r0,=message      //Getting the message's memory address
+	mov r1,#0			//Cleaning register R1
+	mov r2,#0			//additional register which will count the number of passes through the loop
 loop:
-	ldrb r1,[r0], #1    //Dohvatamo prvi karakter i smijestamo ga u registar R1, tj njegovu ascii vrijednost
-						//Nakon toga se vrsi shiftanje
-	cmp r1,#0x00		//Poredimo da li je nas karakter jednak null karakteru (ascii vr. za null karakter je 0)
-	beq endloop			//Ako je nas karakter jednak null karakteru, prekini petlju
-	add r2,r2,#1		//Inkrementiraj pomocni brojac
-	b loop				//Branch (skok) u petlju
+	ldrb r1,[r0], #1    //Getting char value (ascii value)
+						//Shifting
+	cmp r1,#0x00		//Comparing char ascii value with 0 (ascii value for null char is 0)
+	beq endloop			//If r1==0x00 -> break the loop 
+	add r2,r2,#1		//counter++ (additional register)
+	b loop				//Branch
 endloop:
-	mov r1,#0           // test linija za provjeru izlaska iz petlje
-//Vrijednost u registru R2 nakon izvrsavanja petlje je 14 jer odgovarajuci tekst ima tacno toliko karaktera
-// do null karaktera
+	mov r1,#0           // Test line 
